bbefe558c1049022c715341200050c48972ce8e1
bavxhack <44317199+bavxhack@users.noreply.github.com>
2026-09-08 14:05:39 +0200

    Fix fee notice response disposition

--- src/Controller/GebuehrenbescheidDownloadController.php
+++ src/Controller/GebuehrenbescheidDownloadController.php
@@ -10,9 +10,9 @@ use App\Service\ElternService;
 use App\Service\Gebuehrenbescheid\FeeSummaryBuilder;
 use App\Service\Gebuehrenbescheid\PrintGebuehrenbescheidService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\HeaderUtils;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpFoundation\ResponseHeaderBag;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
@@ -69,10 +69,13 @@ final class GebuehrenbescheidDownloadController extends AbstractController
             Response::HTTP_OK,
             ['Content-Type' => 'application/pdf'],
         );
-        $response->headers->setContentDisposition(
-            ResponseHeaderBag::DISPOSITION_ATTACHMENT,
-            $fileName . '.pdf',
-            'gebuehrenbescheid.pdf',
+        $response->headers->set(
+            'Content-Disposition',
+            HeaderUtils::makeDisposition(
+                HeaderUtils::DISPOSITION_ATTACHMENT,
+                $fileName . '.pdf',
+                'gebuehrenbescheid.pdf',
+            ),
         );
 
         return $response;
